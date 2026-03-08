176329294b391167a7e0e3dc1bede5da57ea994e
mymynajud24-a11y <mymynajud24@gmail.com>
2026-03-08 18:05:54 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function register(){
+alert("Account Created Successfully!");
+window.location.href="login.html";
+}
+
+function login(){
+alert("Login Successful!");
+window.location.href="dashboard.html";
+}
+
+function adminLogin(){
+window.location.href="admin.html";
+}
+
+function rent(size,price){
+
+alert("You rented a "+size+" locker for ₱"+price+" per hour.");
+
+}
